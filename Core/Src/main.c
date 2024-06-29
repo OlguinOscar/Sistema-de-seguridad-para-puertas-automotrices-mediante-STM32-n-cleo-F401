1516f8a61e0e1bb21407c535330585332bb786eb
OscarDiaz <diazolguinoscar@gmail.com>
2024-06-28 19:30:41 -0600

    segundo cambio

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -12,245 +12,233 @@
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  *c
   ******************************************************************************
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdio.h>
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+void UART2_Init(void);
+void Keypad_Init(void);
+void GPIO_Init(void);
+void TIM2_Init(void);
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
+TIM_HandleTypeDef htim2;
+GPIO_InitTypeDef GPIO_InitStruct;
 
-/* USER CODE BEGIN PV */
+char keymap[3][3] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'}
+};
 
-/* USER CODE END PV */
+uint32_t row_pins[3] = {GPIO_PIN_11, GPIO_PIN_10, GPIO_PIN_9};
+uint32_t col_pins[3] = {GPIO_PIN_7, GPIO_PIN_6, GPIO_PIN_5};
 
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
-/* USER CODE BEGIN PFP */
+String contrasena = "123456";
+String cadena = "";
 
-/* USER CODE END PFP */
+int contador = 0;
+long inicio = 0;
+bool ya_paso_un_minuto = false;
+char command;
+bool auto_apagado = true;
+bool sasactivado = false;
+int puerta_izquierda;
+int puerta_derecha;
+int cofre;
+int cajuela;
+int check;
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+void delay(){
+    for(int i = 0; i < 400000; i++);
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+int main(void){
+    HAL_Init();
+    UART2_Init();
+    Keypad_Init();
+    GPIO_Init();
+    TIM2_Init();
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+    while(1){
+        check = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+        if(check){
+            auto_apagado = false;
+            check = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3);
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+            printf("prendido\n");
+        } else {
+            printf("apagado\n");
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
+            auto_apagado = true;
+        }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+        if(auto_apagado){
+            int valor = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2);
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+            while(valor){
+                sasactivado = true;
+                valor = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2);
+            }
+
+            while(sasactivado){
+                puerta_izquierda = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_12);
+                puerta_derecha = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_A3);
+                cajuela = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_A0);
+                cofre = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_A1);
+
+                if(puerta_izquierda || puerta_derecha || cajuela || cofre){
+                    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+
+                    while(true){
+                        if(Serial.available() > 0){
+                            command = Serial.read();
+
+                            if(command == '1'){
+                                cadena = "";
+                                sasactivado = false;
+                                contador = 0;
+                                break;
+                            } else if(command == '0'){
+                                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_A2, GPIO_PIN_RESET);
+                            }
+                        }
+
+                        if(contador == 6){
+                            if(cadena == contrasena){
+                                cadena = "";
+                                sasactivado = false;
+                                contador = 0;
+                                break;
+                            } else {
+                                cadena = "";
+                                printf("CONTRASENA EQUIVOCADA\n");
+                                contador = 0;
+                            }
+                        } else {
+                            char keypressed = myKeypad.getKey();
+                            if (keypressed != NO_KEY){
+                                cadena += keypressed;
+                                contador++;
+                            }
+                        }
+                    }
+                    printf("CONTRASENA CORRECTA\n");
+                    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+                }
+            }
+        } else {
+            int valor = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2);
+
+            while(valor){
+                sasactivado = true;
+                valor = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2);
+            }
+
+            while(sasactivado){
+                puerta_izquierda = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_12);
+                puerta_derecha = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_A3);
+                cajuela = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_A0);
+                cofre = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_A1);
+
+                if(puerta_izquierda || puerta_derecha || cajuela || cofre){
+                    while(true){
+                        if(inicio >= 9900 && !ya_paso_un_minuto){
+                            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
+                            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+                            ya_paso_un_minuto = true;
+                        }
+                        if(contador == 6){
+                            if(cadena == contrasena){
+                                cadena = "";
+                                sasactivado = false;
+                                contador = 0;
+                                break;
+                            } else {
+                                cadena = "";
+                                printf("CONTRASENA EQUIVOCADA\n");
+                                contador = 0;
+                            }
+                        } else {
+                            char keypressed = myKeypad.getKey();
+                            if (keypressed != NO_KEY){
+                                cadena += keypressed;
+                                contador++;
+                            }
+                        }
+                        inicio++;
+                        printf("%ld\n", inicio);
+                    }
+
+                    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+                    printf("CONTRASENA CORRECTA\n");
+                    inicio = 0;
+                    ya_paso_un_minuto = false;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
+void UART2_Init(void){
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 9600;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    if(HAL_UART_Init(&huart2) != HAL_OK){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+        while(1);
+    }
 }
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+void Keypad_Init(void){
+    // Initialize GPIO pins for the keypad
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+    for(int i = 0; i < 3; i++){
+        GPIO_InitStruct.Pin = row_pins[i];
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+        GPIO_InitStruct.Pin = col_pins[i];
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    }
 }
 
-/* USER CODE BEGIN 4 */
+void GPIO_Init(void){
+    // Initialize GPIO pins for the system
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 
-/* USER CODE END 4 */
+    GPIO_InitStruct.Pin = GPIO_PIN_A0 | GPIO_PIN_A1 | GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_12 | GPIO_PIN_A3;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_8 | GPIO_PIN_A2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+void TIM2_Init(void){
+    __HAL_RCC_TIM2_CLK_ENABLE();
+    htim2.Instance = TIM2;
+    htim2.Init.Prescaler = 1;
+    htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim2.Init.Period = 0xFFFFFFFF;
+    htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    if(HAL_TIM_Base_Init(&htim2) != HAL_OK){
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+        while(1);
+    }
 }
-#endif /* USE_FULL_ASSERT */
